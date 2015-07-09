5834021088897dabd902ac8d6113127e8b46822f
jeff <jeff.flater@semaphoredoes.com>
2015-07-09 15:06:26 -0400

    seprate the js from html

---
+++ public/js/app.js
@@ -0,0 +1,54 @@
+angular.module("explorer", [])
+    .service('apiService', function ($http) {
+
+        this.makeRequest = function (request) {
+
+            var requestData = {
+                baseUrl: request.baseUrl,
+                restPath: request.restPath,
+                method: request.method,
+                username: request.username,
+                password: request.password,
+                data: request.data
+            };
+
+            return $http({
+                url: '/rest/explorer',
+                method: 'POST',
+                data: requestData
+            }).then(function (result) {
+                    var data = result.data;
+                    return data;
+                },
+                function (result) {
+                    return result;
+                });
+        }
+
+    })
+    .controller('AppCtrl', function AppCtrl($scope, apiService) {
+
+        var exampleData = { fileName: '4%25',
+            attributes:'',
+            drawer:'',
+            fileMarks:'',
+            fileNumber:'',
+            fileType:'' };
+
+        $scope.request = {
+            baseUrl: 'https://con-kblazina2.vertafore.com/ImageRight.Web.Client/',
+            restPath: 'Search/GetResults',
+            method: 'GET',
+            username: '',
+            password: '',
+            data: JSON.stringify(exampleData),
+            response: ''
+        };
+
+        $scope.fireRequest = function () {
+            apiService.makeRequest($scope.request).then(function(result){
+                $scope.request.response = JSON.stringify(result.json);
+            });
+        };
+
+    });
